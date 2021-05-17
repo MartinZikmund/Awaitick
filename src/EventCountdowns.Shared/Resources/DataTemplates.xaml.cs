788385d94db222d6c9611cd88e127f8924052ef8
Martin Zikmund <martin@zikmund.dev>
2021-05-17 12:18:47 +0200

    Started improving main page

---
+++ src/EventCountdowns.Shared/Resources/DataTemplates.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml;
+
+namespace EventCountdowns.Resources
+{
+    public partial class DataTemplates : ResourceDictionary
+    {
+        public DataTemplates()
+        {
+            InitializeComponent();
+        }
+    }
+}
