94f3f5e9e4ccd378f86f1bc1366df6896c60d851
Martin Zikmund <martin@zikmund.dev>
2025-03-02 10:51:12 +0100

    Update

--- src/EventCountdowns.Core/ViewModels/SettingsViewModel.cs
+++ src/EventCountdowns.Core/ViewModels/SettingsViewModel.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventCountdowns.Services.Navigation;
+using EventCountdowns.ViewModels;
 
 namespace EventCountdowns.Core.ViewModels;
 
-public class SettingsViewModel : ViewModelBase
+public partial class SettingsViewModel : PageViewModel
 {
+	public SettingsViewModel(INavigationService navigationService) : base(navigationService)
+	{
+	}
 }
