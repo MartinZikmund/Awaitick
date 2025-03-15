c3ec4f186461d127923312994d09638aaf044ca4
Martin Zikmund <martin@zikmund.dev>
2025-03-15 16:50:29 +0100

    refactor: Remove legacy files

--- src/old/EventCountdowns/Resources/ColorResources.cs
+++
@@ -1,8 +0,0 @@
-﻿using Windows.UI;
-
-namespace EventCountdowns.Core.Resources;
-
-public class ColorResources
-{
-	public static Color BrandColor => ResourceAccessor.GetResource<Color>("BrandColor");
-}
