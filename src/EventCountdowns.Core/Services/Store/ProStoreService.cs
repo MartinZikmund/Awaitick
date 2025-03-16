3ff959d8c215bb98b20a9aed5d194e85f97fdbea
Martin Zikmund <martin@zikmund.dev>
2025-03-16 09:05:47 +0100

    feat: Basic settings page

--- src/EventCountdowns.Core/Services/Store/ProStoreService.cs
+++ src/EventCountdowns.Core/Services/Store/ProStoreService.cs
@@ -6,7 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EventCountdowns.Services.Store;
-internal class ProStoreService : IStoreService
+
+public class ProStoreService : IStoreService
 {
 	public Task<string?> GetPriceAsync() => Task.FromResult("Nada");
 	public Task<bool> HasProAsync() => Task.FromResult(true);
