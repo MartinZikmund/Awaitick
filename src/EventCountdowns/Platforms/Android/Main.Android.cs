aebabc6c0fae8f33050336bf098b6147fea59df7
Martin Zikmund <martin@zikmund.dev>
2025-04-16 19:28:15 +0200

    chore: Adjust Android

--- src/EventCountdowns/Platforms/Android/Main.Android.cs
+++ src/EventCountdowns/Platforms/Android/Main.Android.cs
@@ -8,7 +8,6 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Com.Nostra13.Universalimageloader.Core;
 using Microsoft.UI.Xaml.Media;
 using static Android.Telephony.CarrierConfigManager;
 
@@ -28,21 +27,8 @@ public class Application : Microsoft.UI.Xaml.NativeApplication
 	}
 
 	public Application(IntPtr javaReference, JniHandleOwnership transfer)
-        : base(() => new CountdownsApp(), javaReference, transfer)
-    {
-        ConfigureUniversalImageLoader();
-    }
-
-    private static void ConfigureUniversalImageLoader()
-    {
-        // Create global configuration and initialize ImageLoader with this config
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration
-            .Builder(Context)
-            .Build();
-
-        ImageLoader.Instance.Init(config);
-
-        ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
-    }
+		: base(() => new CountdownsApp(), javaReference, transfer)
+	{
+	}
 }
 
