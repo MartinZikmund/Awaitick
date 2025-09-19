9c21ec24ad26be09aa856d1ab838d20034b100cd
Martin Zikmund <martin@zikmund.dev>
2025-09-19 14:41:28 +0200

    feat: Enhance onboarding and event display handling
    Refactor onboarding flow to improve user experience:
    - Removed `ViewLoaded` and replaced it with `StartAppAsync`.
    - Added `CreateCustomEventAsync` for navigating to custom event creation.
    - Updated `SelectedPresets` to initialize with an empty array.
    - Replaced "Save" and "Skip" buttons with a single "Continue" button.
    
    Improve `EventCountdownDisplayControl`:
    - Adjusted visibility logic for `TargetDateTimeTextBlock`.
    - Added `x:Name` attributes for better UI element identification.
    
    Add new localization strings for onboarding in default and Czech locales.
    
    Refactor `OnboardingView.xaml`:
    - Reformatted styles for better readability.
    - Added new onboarding step for creating custom events.

--- src/Awaitick.Core/ViewModels/OnboardingViewModel.cs
+++ src/Awaitick.Core/ViewModels/OnboardingViewModel.cs
@@ -17,39 +17,39 @@ public partial class OnboardingViewModel : PageViewModel
 		_dataService = dataService;
 	}
 
-	public override void ViewLoaded()
-	{
-		_appPreferences.FirstStart = false;
-	}
-
 	public EventPreset[] Presets => EventPresets.Presets;
 
-	public EventPreset[] SelectedPresets { get; set; }
+	public EventPreset[] SelectedPresets { get; set; } = Array.Empty<EventPreset>();
 
 	[ObservableProperty]
 	public partial int Step { get; set; } = 0;
 
 	[RelayCommand]
-	public void NextStep() => Step = 1;
+	private void NextStep() => Step += 1;
 
 	[RelayCommand]
-	public void SkipPresets() => StartApp();
-
-	[RelayCommand]
-	public async Task SavePresets()
+	private async Task CreateCustomEventAsync()
 	{
-		foreach(var preset in SelectedPresets)
+		await StartAppAsync();
+
+		NavigationService.Navigate<CountdownEditorViewModel>(CountdownEditorViewModel.NavigationModel.CreateAdd());
+	}
+
+	[RelayCommand]
+	private async Task StartAppAsync()
+	{
+		await SavePresetsAsync();
+		
+		_appPreferences.FirstStart = false;
+		NavigationService.Navigate<MainViewModel>();
+	}
+
+	private async Task SavePresetsAsync()
+	{
+		foreach (var preset in SelectedPresets)
 		{
 			var eventCountdown = preset.Create();
 			await _dataService.AddCountdownAsync(eventCountdown);
 		}
-
-		StartApp();
-	}
-
-	private void StartApp()
-	{
-		_appPreferences.FirstStart = false;
-		NavigationService.Navigate<MainViewModel>();
 	}
 }
